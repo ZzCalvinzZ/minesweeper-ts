29aea5b30b81d73c55b14fe098345ad203aaf652
Calvin Collins <calvinc@surveymonkey.com>
2020-10-02 15:51:17 -0400

    add revealCell and tests

---
+++ src/utils/__tests__/mines.ts
@@ -0,0 +1,173 @@
+import { generateMinefield, revealCell } from "../mines";
+import { Minefield } from "../../GameContext";
+
+const stringUpMinefield = (minefield: Minefield) => {
+  return minefield
+    .map((col) =>
+      col
+        .map((cell) =>
+          cell.hasMine
+            ? "x"
+            : cell.state === 0
+            ? "U"
+            : cell.surroundingMines || 0
+        )
+        .join()
+    )
+    .join("\n");
+};
+
+describe("mines.ts", () => {
+  test("minefield generates", () => {
+    const field = generateMinefield(9, 9, 10);
+    console.log(field);
+    const stringField = field
+      .map((col) => col.map((cell) => (cell.hasMine ? "x" : "0")).join())
+      .join("\n");
+    console.log(stringField);
+  });
+
+  test("reveal cell", () => {
+    const minefield: Minefield = [
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+      ],
+      [
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: false },
+        { state: 0, hasMine: true },
+        { state: 0, hasMine: false },
+      ],
+    ];
+
+    /*
+     *
+    0,0,x,0,x,0,0,0,0
+    0,0,0,0,0,0,0,0,0
+    0,0,0,x,0,0,0,0,0
+    0,0,0,0,0,0,0,0,0
+    0,x,x,0,0,0,0,0,0
+    0,x,0,0,0,0,x,0,0
+    0,0,x,0,0,0,0,0,0
+    0,0,0,0,0,0,0,0,0
+    0,x,0,0,0,0,0,x,0
+
+    */
+
+    revealCell(0, 3, minefield);
+    expect(stringUpMinefield(minefield)).toEqual(
+      `U,U,x,2,x,U,U,U,U
+U,U,U,U,U,U,U,U,U
+U,U,U,x,U,U,U,U,U
+U,U,U,U,U,U,U,U,U
+U,x,x,U,U,U,U,U,U
+U,x,U,U,U,U,x,U,U
+U,U,x,U,U,U,U,U,U
+U,U,U,U,U,U,U,U,U
+U,x,U,U,U,U,U,x,U`
+    );
+
+    revealCell(0, 0, minefield);
+    expect(stringUpMinefield(minefield)).toEqual(
+      `0,1,x,2,x,U,U,U,U
+0,1,2,U,U,U,U,U,U
+0,0,1,x,U,U,U,U,U
+1,2,3,U,U,U,U,U,U
+U,x,x,U,U,U,U,U,U
+U,x,U,U,U,U,x,U,U
+U,U,x,U,U,U,U,U,U
+U,U,U,U,U,U,U,U,U
+U,x,U,U,U,U,U,x,U`
+    );
+  });
+});
--- src/utils/mines.ts
+++ src/utils/mines.ts
@@ -1,6 +1,6 @@
 import { DEFAULT_CELL } from "../constants";
 import { getRandomInt } from "./random";
-import { Minefield } from "../GameContext";
+import { Minefield, CellState } from "../GameContext";
 
 export const generateMinefield = (
   columns: number,
@@ -36,4 +36,51 @@ export const generateMinefield = (
     });
 
   return minefield;
+};
+
+const getCell = (column: number, row: number, minefield: Minefield) => {
+  if (!minefield[column]) return null;
+  if (!minefield[column][row]) return null;
+
+  return minefield[column][row];
+};
+
+export const revealCell = (
+  column: number,
+  row: number,
+  minefield: Minefield
+) => {
+  const columns = [column - 1, column, column + 1];
+  const rows = [row - 1, row, row + 1];
+  const mainCell = getCell(column, row, minefield);
+
+  // if cell is out of bounds or opened already
+  if (!mainCell) return;
+  if (mainCell.state === CellState.Opened) return;
+
+  // set the cell as opened
+  mainCell.state = CellState.Opened;
+
+  let surroundingMines: number = 0;
+
+  // check surrounding cells for mine
+  columns.forEach((c) => {
+    rows.forEach((r) => {
+      const cell = getCell(c, r, minefield);
+      if (!cell) return;
+      if (cell.state === CellState.Opened) return;
+      if (cell.hasMine) surroundingMines = surroundingMines + 1;
+    });
+  });
+
+  minefield[column][row].surroundingMines = surroundingMines;
+
+  if (!surroundingMines) {
+    //reveal surrounding cells
+    columns.forEach((c) => {
+      rows.forEach((r) => {
+        revealCell(c, r, minefield);
+      });
+    });
+  }
 };
