1bd210f8fdf3105e7a355637b832d35c755b311c
Calvin Collins <calvinc@surveymonkey.com>
2020-12-16 10:33:29 -0500

    Updates

---
+++ precache-manifest.fa40e7bced4284b922ae1725ff6f60ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac57d5c00061635f70fad95db2f0f4f0",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "98a9e94b59aeefd25b7a",
+    "url": "/minesweeper/static/js/2.3a344d66.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/minesweeper/static/js/2.3a344d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004275baf27076385e09",
+    "url": "/minesweeper/static/js/main.fec2d3f1.chunk.js"
+  },
+  {
+    "revision": "eb03e9f38a452c459ff6",
+    "url": "/minesweeper/static/js/runtime-main.aad984f9.js"
+  },
+  {
+    "revision": "bff5c9fc8a36d1f0150b9f0d7cdb4a5c",
+    "url": "/minesweeper/static/media/flag.bff5c9fc.png"
+  }
+]);
